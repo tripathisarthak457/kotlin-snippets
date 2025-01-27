c4a8a6cb99054b613f379fa43f905162d1e5c7d6
tripathisarthak457 <98253995+tripathisarthak457@users.noreply.github.com>
2025-01-27 19:28:28 +0530

    freq used kotlin code - utils, composables, viewmodels, components etc

---
+++ EmailValidation.kt
@@ -0,0 +1,9 @@
+fun isValidEmail(email: String): Boolean {
+    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
+    return email.matches(emailRegex)
+}
+
+fun main() {
+    val email = "example@example.com"
+    println("Is the email valid? ${isValidEmail(email)}")
+}
